dea99fd25c742e952b24598517a2c9c5e0a3d9fb
agil-mix-catalogo <agilmixjeans@gmail.com>
2026-09-07 17:33:34 -0300

    Add files via upload

--- app_catalogo_web.py
+++ app_catalogo_web.py
@@ -284,18 +284,21 @@ def from_json_filter(s):
 
 @app.route('/ver_imagem_id/<int:prod_id>')
 def ver_imagem_id(prod_id):
-  pasta_produtos = os.path.join('imagens', 'produtos')
-  if not os.path.exists(pasta_produtos):
+  # Varre recursivamente todas as pastas e subpastas dentro de 'imagens/produtos'
+  raiz_produtos = os.path.join('imagens', 'produtos')
+  if not os.path.exists(raiz_produtos):
     return '', 404
 
   extensoes = ['.jpg', '.jpeg', '.png', '.webp', '.JPG', '.JPEG', '.PNG']
-  for ext in extensoes:
-    caminho = os.path.join(pasta_produtos, f'{prod_id}{ext}')
-    if os.path.exists(caminho):
-      response = make_response(send_file(caminho))
-      # Desativa o cache do navegador para nunca carregar imagem antiga
-      response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-      return response
+
+  for pasta_atual, _, arquivos in os.walk(raiz_produtos):
+    for ext in extensoes:
+      nome_procurado = f'{prod_id}{ext}'
+      if nome_procurado in arquivos:
+        caminho_arquivo = os.path.join(pasta_atual, nome_procurado)
+        response = make_response(send_file(caminho_arquivo))
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        return response
 
   return '', 404
 
